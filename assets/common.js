fa3247a76362bc966b28649cdb1a1e27e7334fc5
gyosit <shota.y2011@gmail.com>
2021-04-04 21:18:41 +0900

    play with PIXI

--- assets/common.js
+++ assets/common.js
@@ -1,5 +1,15 @@
 window.addEventListener("resize", function() {
-   app.renderer.resize(window.innerWidth, window.innerHeight);
-   gobanSprit.renderer.resize(window.innerWidth, window.innerHeight);
+   window_w = window.innerWidth;
+   window_h = window.innerHeight;
+   app.renderer.resize(window_w-10, window_h-10);
+   app.stage.removeChildren();
+   if (window_w < window_h){
+      SIDE = window_w - margin;
+   }else{
+      SIDE = window_h - margin;
+   }
+   SIDE -= 10;
+
+   initGoban(app, board_size, texture, senrigan);
 });
 
