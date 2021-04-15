03f10a95deba6a9757efc3cc2ce2d3bed2a6f80a
root <root@ik1-449-56596.vs.sakura.ne.jp>
2021-04-15 13:59:20 +0900

    release

--- secure/secure.go
+++ secure/secure.go
@@ -2,8 +2,8 @@ package secure
 
 func Init() (string, string, string, string){
 	DBMS := "mysql"
-	USER := "igo"
-	PASS := "Ss4071132019_igo"
-	DBNAME := "igo?parseTime=true"
+	USER := "igo_goiro"
+	PASS := "Ss40goiro219"
+	DBNAME := "igo_goiro?parseTime=true"
 	return DBMS, USER, PASS, DBNAME
 }
