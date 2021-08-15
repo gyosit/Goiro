fc7d4d8c90f72a94d951428866efe25b69ab9a33
gyosit <shota.y2011@gmail.com>
2021-08-15 17:16:46 +0900

    hide some files for security

--- secure/secure.go
+++
@@ -1,9 +0,0 @@
-package secure
-
-func Init() (string, string, string, string){
-	DBMS := "mysql"
-	USER := "igo_goiro"
-	PASS := "Ss40goiro219"
-	DBNAME := "igo_goiro?parseTime=true"
-	return DBMS, USER, PASS, DBNAME
-}
